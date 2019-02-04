8f084c7432149fd6a5c72cc25ea35348ebeba055
Carine Beatrici <carine_beatrici@yahoo.com.br>
2019-02-04 14:51:42 +0100

    Add files via upload

---
+++ include/param.h
@@ -0,0 +1,49 @@
+#define TIME_FINAL 100000
+#define SNAPSHOT 1000
+#define L_TENTATIVA 36
+
+#define QUADRADO 0
+#define CIRCULO 1
+#define CANTO 2
+
+#define SORTEIO CANTO
+
+#define V1 0.05
+
+#define R_MAX  1.00    //1.0  //0.55
+#define R_CORE 0.18    //0.36 //0.2
+#define R_EQ   0.80    //0.73 //0.4
+
+
+
+
+#define RESEVOIR_DENSITY .558 / R_CORE
+
+
+#define ALPHA   0.25  / V1
+#define BETA    1.75 
+#define ETA     1.0
+
+
+
+#define F_0 1000.0
+
+#define PI 3.14159265
+#define LOAD 0
+
+#define Lx  200
+#define Ly  30
+
+#define OBSTACULO 1
+#define PAREDE    1
+
+#define L_CANTO  1.0
+#define R_ESFERA 4.5
+
+#define R_FRONT 5.0
+
+#define L_CENTRO_X 45
+#define L_CENTRO_Y 15
+
+#define N 50000
+
